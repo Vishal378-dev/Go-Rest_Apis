bdab1eb60ea0ff53d418a2343b5c26126f4fb73b
Vishal <visahlsingh0512@gmail.com>
2024-12-07 12:26:00 +0530

    CRUD

--- DB/db.go
+++ DB/db.go
@@ -3,3 +3,5 @@ package db
 type DB interface {
 	CreateEmployee(empid string, name string, email string, phonenumber int, salary int) (string, error)
 }
+
+// getBYID where r.pathvalue("id") --> {id } -->queryrow.scan(&parameter you want ot grab)  sql.errnorows
